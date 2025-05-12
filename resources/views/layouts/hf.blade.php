ddd8cdb837a24b54346985f12060ae1afce4d50d
Dendy <102230690+SOSISKA0000000007@users.noreply.github.com>
2025-05-12 20:57:32 +0300

    верстка попытка третья

--- resources/views/layouts/hf.blade.php
+++ resources/views/layouts/hf.blade.php
@@ -11,7 +11,6 @@
 
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 @include('layouts.header')
